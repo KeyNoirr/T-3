6871070c33947269861e4cafb0c5bdcce9be0c4f
TRAN CAO SON <15dh110206@st.huflit.edu.vn>
2018-03-23 17:26:14 +0700

    Add project files.

---
+++ T3/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace T3
+{
+    partial class FrmClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labYourS = new System.Windows.Forms.Label();
+            this.btnKeo = new System.Windows.Forms.Button();
+            this.btnBua = new System.Windows.Forms.Button();
+            this.btnBao = new System.Windows.Forms.Button();
+            this.labYourResult = new System.Windows.Forms.Label();
+            this.txtYourS = new System.Windows.Forms.TextBox();
+            this.txtYourR = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labYourS
+            // 
+            this.labYourS.AutoSize = true;
+            this.labYourS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labYourS.Location = new System.Drawing.Point(120, 60);
+            this.labYourS.Name = "labYourS";
+            this.labYourS.Size = new System.Drawing.Size(145, 25);
+            this.labYourS.TabIndex = 0;
+            this.labYourS.Text = "Your Selection:";
+            // 
+            // btnKeo
+            // 
+            this.btnKeo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeo.Location = new System.Drawing.Point(123, 187);
+            this.btnKeo.Name = "btnKeo";
+            this.btnKeo.Size = new System.Drawing.Size(110, 50);
+            this.btnKeo.TabIndex = 2;
+            this.btnKeo.Text = "Kéo";
+            this.btnKeo.UseVisualStyleBackColor = true;
+            this.btnKeo.Click += new System.EventHandler(this.btnKeo_Click);
+            // 
+            // btnBua
+            // 
+            this.btnBua.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBua.Location = new System.Drawing.Point(297, 187);
+            this.btnBua.Name = "btnBua";
+            this.btnBua.Size = new System.Drawing.Size(110, 50);
+            this.btnBua.TabIndex = 3;
+            this.btnBua.Text = "Búa";
+            this.btnBua.UseVisualStyleBackColor = true;
+            this.btnBua.Click += new System.EventHandler(this.btnBua_Click);
+            // 
+            // btnBao
+            // 
+            this.btnBao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBao.Location = new System.Drawing.Point(479, 187);
+            this.btnBao.Name = "btnBao";
+            this.btnBao.Size = new System.Drawing.Size(110, 50);
+            this.btnBao.TabIndex = 4;
+            this.btnBao.Text = "Bao";
+            this.btnBao.UseVisualStyleBackColor = true;
+            this.btnBao.Click += new System.EventHandler(this.btnBao_Click);
+            // 
+            // labYourResult
+            // 
+            this.labYourResult.AutoSize = true;
+            this.labYourResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labYourResult.Location = new System.Drawing.Point(120, 334);
+            this.labYourResult.Name = "labYourResult";
+            this.labYourResult.Size = new System.Drawing.Size(118, 25);
+            this.labYourResult.TabIndex = 7;
+            this.labYourResult.Text = "Your Result:";
+            // 
+            // txtYourS
+            // 
+            this.txtYourS.Location = new System.Drawing.Point(297, 60);
+            this.txtYourS.Multiline = true;
+            this.txtYourS.Name = "txtYourS";
+            this.txtYourS.Size = new System.Drawing.Size(100, 25);
+            this.txtYourS.TabIndex = 8;
+            // 
+            // txtYourR
+            // 
+            this.txtYourR.Location = new System.Drawing.Point(244, 334);
+            this.txtYourR.Multiline = true;
+            this.txtYourR.Name = "txtYourR";
+            this.txtYourR.Size = new System.Drawing.Size(100, 25);
+            this.txtYourR.TabIndex = 10;
+            // 
+            // FrmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 425);
+            this.Controls.Add(this.txtYourR);
+            this.Controls.Add(this.txtYourS);
+            this.Controls.Add(this.labYourResult);
+            this.Controls.Add(this.btnBao);
+            this.Controls.Add(this.btnBua);
+            this.Controls.Add(this.btnKeo);
+            this.Controls.Add(this.labYourS);
+            this.Name = "FrmClient";
+            this.Text = "Clients";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmClient_FormClosing);
+            this.Load += new System.EventHandler(this.FrmClient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labYourS;
+        private System.Windows.Forms.Button btnKeo;
+        private System.Windows.Forms.Button btnBua;
+        private System.Windows.Forms.Button btnBao;
+        private System.Windows.Forms.Label labYourResult;
+        private System.Windows.Forms.TextBox txtYourS;
+        private System.Windows.Forms.TextBox txtYourR;
+    }
+}
+
---
+++ T3/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T3
+{
+    public partial class FrmClient : Form
+    {
+        Socket servers;
+        EndPoint remote;
+        public FrmClient()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmClient_Load(object sender, EventArgs e)
+        {
+            servers = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            remote = (EndPoint)ipe;
+        }
+
+        private void btnKeo_Click(object sender, EventArgs e)
+        {
+            txtYourS.Text = "Kéo";
+            byte[] sendData = Encoding.ASCII.GetBytes("0");
+            servers.SendTo(sendData, remote);
+            byte[] receiveData = new byte[20];
+            servers.ReceiveFrom(receiveData, ref remote);
+            int recv = Convert.ToInt32(Encoding.ASCII.GetString(receiveData));
+
+            if (recv == 0)
+                txtYourR.Text = "Hòa";
+            else if (recv == 1)
+                txtYourR.Text = "Thua";
+            else
+                txtYourR.Text = "Thắng";
+        }
+
+
+        private void btnBua_Click(object sender, EventArgs e)
+        {
+            txtYourS.Text = "Búa";
+            byte[] sendData = Encoding.ASCII.GetBytes("1");
+            servers.SendTo(sendData, remote);
+            byte[] receiveData = new byte[20];
+            servers.ReceiveFrom(receiveData, ref remote);
+            int recv = Convert.ToInt32(Encoding.ASCII.GetString(receiveData));
+
+            if (recv == 0)
+                txtYourR.Text = "Thắng";
+            else if (recv == 1)
+                txtYourR.Text = "Hòa";
+            else
+                txtYourR.Text = "Thua";
+        }
+
+
+        private void btnBao_Click(object sender, EventArgs e)
+        {
+            txtYourS.Text = "Bao";
+            byte[] sendData = Encoding.ASCII.GetBytes("2");
+            servers.SendTo(sendData, remote);
+            byte[] receiveData = new byte[20];
+            servers.ReceiveFrom(receiveData, ref remote);
+            int recv = Convert.ToInt32(Encoding.ASCII.GetString(receiveData));
+
+            if (recv == 0)
+                txtYourR.Text = "Thua";
+            else if (recv == 1)
+                txtYourR.Text = "Thắng";
+            else
+                txtYourR.Text = "Hòa";
+        }
+
+        private void FrmClient_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            servers.Close();
+        }
+    }
+}
